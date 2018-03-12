dbd02f19d653c8fa45a81133823b93d0527be80a
AlexandrSher <30312859+AlexandrSher@users.noreply.github.com>
2018-03-12 08:14:26 +0200

    Update task2.py
    Added input from console.

--- task2.py
+++ task2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 '''task2'''
 # pylint: disable= invalid-name
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+print('Add first list:')
+a = input().split()
+print('Add second list:')
+b = input().split()
 s = []
 
 
