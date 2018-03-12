e9872adbfeb99c8ea5fdc42a2f2dbfe79453d535
AlexandrSher <30312859+AlexandrSher@users.noreply.github.com>
2018-03-12 07:55:27 +0200

    Update task1.py
    Done it.

--- task1.py
+++ task1.py
@@ -6,24 +6,6 @@ year = int(input())
 
 def is_leap(yr):
     '''task1'''
-    leap = False
-    if yr % 4 != 0 or (yr % 400 != 0 and yr % 100 == 0):
-        leap = False
-    else:
-        leap = True
-
-    return leap
-
-
+    return yr % 4 != 0 or (yr % 400 != 0 and yr % 100 == 0)
+   
 print(is_leap(year))
-
-
-if year % 4 != 0:
-    print('no')
-elif year % 100 == 0:
-    if year % 400 == 0:
-        print('yes')
-    else:
-        print('no')
-else:
-    print('yes')
